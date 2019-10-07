988a6f36e6a4fa2be9580782e6f9c6e734c00a5b
eleex <eleex1988@gmail.com>
2019-10-07 19:32:32 +0300

    cat-energy_gulp-setup

---
+++ gulpfile.js
@@ -0,0 +1,58 @@
+const gulp = require('gulp');
+const del = require('del');
+const plumber = require('gulp-plumber');
+const sass = require('gulp-sass');
+const sourcemaps = require('gulp-sourcemaps');
+const autoprefixer = require('gulp-autoprefixer');
+const embedSvg = require('gulp-embed-svg');
+const browserSync = require('browser-sync').create();
+
+sass.compiler = require('node-sass');
+
+function clear () {
+    return del('./build/*');
+}
+
+function styles () {
+    return gulp.src('./src/sass/main-page.scss')
+                .pipe(plumber())
+                .pipe(sass().on('error', sass.logError))
+                .pipe(sourcemaps.init())
+                .pipe(autoprefixer({
+                    overrideBrowserslist: ['> 0.1%'],
+                    cascade: false
+                }))
+                // .pipe(gcmq())
+                .pipe(sourcemaps.write())
+                .pipe(gulp.dest('./build/css'))
+                .pipe(browserSync.stream());
+}
+
+function img () {
+    return gulp.src('./src/img/**/*')
+                .pipe(gulp.dest('./build/img'));
+}
+
+function html () {
+    return gulp.src('./src/*.html')
+                .pipe(embedSvg({
+                    root: './build/'
+                  }))
+                .pipe(gulp.dest('./build/'))
+                .pipe(browserSync.stream());
+}
+
+function watch () {
+    browserSync.init({
+        server: {
+            baseDir: "./build/"
+        }
+    });
+
+    gulp.watch('./src/sass/**/*.scss', styles);
+    gulp.watch('./src/*.html', html);
+}
+
+let build = gulp.series(clear, gulp.parallel(styles, img), html, watch);
+
+gulp.task('build', build);
