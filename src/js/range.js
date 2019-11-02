689edd4ae09e43551bb8c901a490e8e578e9866d
eleex <eleex1988@gmail.com>
2019-11-02 20:40:08 +0200

    input-range style

--- src/js/range.js
+++ src/js/range.js
@@ -5,5 +5,9 @@ var imgCatAfter = document.querySelector(".example__cat-after");
 function showRange(){
     imgCatBefore.style.width = range.value + "%";
     imgCatAfter.style.width = 100 - range.value + "%";
-    console.log(range.value);
+    range.style.background = 'linear-gradient(90deg, rgb(104, 183, 56) ' 
+                                + range.value + '%, ' 
+                                + 'rgb(220, 220, 220) ' + range.value + '%' + ')';
 }
+
+range.addEventListener("input", showRange);
